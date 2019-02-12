3b5b33d247030901ee651bd5e2ed2e18ce99056b
Alex Jabiro <alexjabiro@gmail.com>
2019-02-11 20:03:06 -0500

    I updated the BeerSong file to be a complete code.

--- BeerSong.java
+++ BeerSong.java
@@ -1,9 +1,28 @@
 public class Main {
 
     public static void main(String[] args) {
-
+	Ninety_Nine_Bottles_of_Beer();
     }
     public static void Ninety_Nine_Bottles_of_Beer() {
-        
+         int i = 99;
+        String name = "bottles";
+
+        while (i > 0) {
+
+            System.out.println(i + " " + name + " of beer on the wall, " + i + " " + name + " of beer");
+            i = i - 1;
+
+            if (i == 1) {
+                name = "bottle";
+            }
+
+            if (i > 0) {
+                System.out.println("take one down, pass it around, " + i + " " + name + " of beer of beer on the wall.");
+            }
+        }
+
+        if (i == 0) {
+            System.out.println(", no more bottles of beer on the wall.");
+        }
     }
 }
